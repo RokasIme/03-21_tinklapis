779b588e98a97fdb82acbce90e8519e53234e028
Rokas <Rokas.Imelinskas@gmail.com>
2025-03-24 11:33:38 +0200

    web design

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+const mobileNav = document.querySelector(".header__main-nav");
 const mobileNavOpen = document.querySelector(".mobile-nav__open");
 
 mobileNavOpen.addEventListener("click", () => {
   mobileNavOpen.classList.toggle("active");
+  mobileNav.classList.toggle("active");
 });
